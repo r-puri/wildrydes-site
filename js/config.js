34dded6350e028173de7d83609110a49c1bd6a87
r-puri <rohanpuri@gmail.com>
2022-05-22 12:00:12 -0700

    cognito settings

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-1_LmM3itQTj', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1688a9csvlmv8qmlhhos3d70up', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-1_KeZJkQ1ZV', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3ptedhfp2q07dtq4nu2v846a12', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-1' // e.g. us-east-2
     },
     api: {
